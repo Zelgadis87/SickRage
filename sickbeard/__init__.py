cac0773b026d00903452c40e72b262f0bad4fa8f
Gabriele Tomberli <gabriele.tomberli@gmail.com>
2016-06-19 22:46:34 +0200

    Reduced back trakt checker interval to 2 hours

--- sickbeard/__init__.py
+++ sickbeard/__init__.py
@@ -1439,7 +1439,7 @@ def initialize(consoleLogging=True):
                                                          silent=not PROCESS_AUTOMATICALLY)
 
         traktCheckerScheduler = scheduler.Scheduler(traktChecker.TraktChecker(),
-                                                    cycleTime=datetime.timedelta(hours=4),
+                                                    cycleTime=datetime.timedelta(hours=2),
                                                     threadName="TRAKTCHECKER",
                                                     silent=not USE_TRAKT)
 
